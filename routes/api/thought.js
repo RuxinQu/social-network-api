46e4779b7f6cf7373e3458345116ccd8a46f909b
Ruxin Qu <rosheenqu@gmail.com>
2022-12-14 17:43:11 -0800

    allow add and delete reactions

--- routes/api/thought.js
+++ routes/api/thought.js
@@ -1,12 +1,13 @@
 const router = require('express').Router();
 const { getAllThought, getOneThought, postThought, deleteThought, updateThought } = require('../../controllers/thoughtController')
+const { addReaction, deleteReaction } = require('../../controllers/reactionController')
 
 router.get('/', getAllThought)
     .post('/', postThought)
     .get('/:id', getOneThought)
     .put('/:id', updateThought)
     .delete('/:id', deleteThought)
-
-
+    .post('/:thoughtId/reactions', addReaction)
+    .delete('/:thoughtId/reactions/:reactionId', deleteReaction)
 
 module.exports = router;
