f6cf9e8ef2547c005fa8527ddb4519b9949f30c2
Ruxin Qu <rosheenqu@gmail.com>
2022-12-15 19:46:46 -0800

    for add friends : change $push to $addToSet to filter out duplicate data

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,13 +1,18 @@
 const router = require('express').Router();
 const { getAllUser, getOneUser, postUser, updateUser, deleteUser, addFriend, deleteFriend } = require('../../controllers/userController')
 
-router.get('/', getAllUser)
-    .post('/', postUser)
-    .get('/:id', getOneUser)
-    .put('/:id', updateUser)
-    .delete('/:id', deleteUser)
-    .post('/:userId/friends/:friendId', addFriend)
-    .delete('/:userId/friends/:friendId', deleteFriend)
+router.route('/')
+    .get(getAllUser)
+    .post(postUser)
+
+router.route('/:id')
+    .get(getOneUser)
+    .put(updateUser)
+    .delete(deleteUser)
+    
+router.route('/:userId/friends/:friendId')
+    .post(addFriend)
+    .delete(deleteFriend)
 
 
 module.exports = router;
