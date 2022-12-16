fb491d7ab9d9e803d60c5ff1a51bdcea554926bc
Ruxin Qu <rosheenqu@gmail.com>
2022-12-15 19:38:37 -0800

    for add friends : change $push to $addToSet to filter out duplicate data

--- routes/api/thought.js
+++ routes/api/thought.js
@@ -2,12 +2,16 @@ const router = require('express').Router();
 const { getAllThought, getOneThought, postThought, deleteThought, updateThought } = require('../../controllers/thoughtController')
 const { addReaction, deleteReaction } = require('../../controllers/reactionController')
 
-router.get('/', getAllThought)
-    .post('/', postThought)
-    .get('/:id', getOneThought)
-    .put('/:id', updateThought)
-    .delete('/:id', deleteThought)
-    .post('/:thoughtId/reactions', addReaction)
+router.route('/')
+    .get(getAllThought)
+    .post(postThought)
+
+router.route('/:id')
+    .get(getOneThought)
+    .put(updateThought)
+    .delete(deleteThought)
+
+router.post('/:thoughtId/reactions', addReaction)
     .delete('/:thoughtId/reactions/:reactionId', deleteReaction)
 
 module.exports = router;
